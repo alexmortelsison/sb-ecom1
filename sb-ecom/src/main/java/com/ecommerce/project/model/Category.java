6ce52424f474d4cf6ffbaef1c17faf589296d86d
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-07 16:24:03 +0900

    added Service

--- sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -1,27 +1,27 @@
 package com.ecommerce.project.model;
 
 public class Category {
-    private Long categoryid;
-    private String categoryname;
+    private Long categoryId;
+    private String categoryName;
 
     public Category(Long categoryid, String categoryname) {
-        this.categoryid = categoryid;
-        this.categoryname = categoryname;
+        this.categoryId = categoryid;
+        this.categoryName = categoryname;
     }
 
-    public Long getCategoryid() {
-        return categoryid;
+    public Long getCategoryId() {
+        return categoryId;
     }
 
-    public void setCategoryid(Long categoryid) {
-        this.categoryid = categoryid;
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
     }
 
-    public String getCategoryname() {
-        return categoryname;
+    public String getCategoryName() {
+        return categoryName;
     }
 
-    public void setCategoryname(String categoryname) {
-        this.categoryname = categoryname;
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
     }
 }
