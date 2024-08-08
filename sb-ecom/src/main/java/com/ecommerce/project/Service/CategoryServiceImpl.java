84a2d89022b7ac655c2d5828dcba2f0ca97d1bb4
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-08 11:19:22 +0900

    updated Service

--- sb-ecom/src/main/java/com/ecommerce/project/Service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Service/CategoryServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    List<Category> categories = new ArrayList<>();
+    private final List<Category> categories = new ArrayList<>();
 
     @Override
     public List<Category> getAllCategories() {
